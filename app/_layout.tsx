c549aba9f334d15221dd7404de442f937cdb425c
Teona <tekizashvili@gmail.com>
2025-02-28 18:21:22 +0400

    updated root files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,19 @@
 import { Stack } from 'expo-router';
 import { Provider } from 'react-redux';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import { store } from '@/store/store';
 
-export default function RootLayout() {
+export default function RootLayout(): JSX.Element {
 	return (
 		<Provider store={store}>
-			<Stack>
-				<Stack.Screen name='index' options={{ headerShown: false }} />
-			</Stack>
+			<SafeAreaProvider>
+				<StatusBar style='dark' />
+				<Stack>
+					<Stack.Screen name='index' options={{ headerShown: false }} />
+				</Stack>
+			</SafeAreaProvider>
 		</Provider>
 	);
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,45 +1,57 @@
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { SafeAreaView, ScrollView } from 'react-native';
+import { useCallback, useState } from 'react';
+import { SafeAreaView, ScrollView, View } from 'react-native';
 
 import { Day } from '@/types/events';
 import { RootState } from '@/store/store';
-import useEvents from '@/hooks/useEvents';
+import useEvents from '@/hooks/events/useEvents';
+import useResetEvents from '@/hooks/useResetEvents';
+import homePageStyles from '@/styles/homePage.styles';
+import ButtonGroup from '@/components/buttons/TestButtons';
+import useCreatePastEvents from '@/hooks/useCreatePastEvents';
 import CalendarComponent from '@/components/calendar/CalendarComponent';
-import EventListComponent from '@/components/calendar/EventListComponent';
+import EventListComponent from '@/components/events/EventListComponent';
 
-export default function HomePage() {
+export default function HomePage(): JSX.Element {
 	const [showEventForm, setShowEventForm] = useState(false);
-
 	const { markedDates, selectedDate } = useSelector(
 		(state: RootState) => state.calendar
 	);
-	const { selectedDateEvents, onDayPress } = useEvents();
+	const { onDayPress } = useEvents();
+	const { resetAllEvents } = useResetEvents();
+	const { createPastEvents } = useCreatePastEvents();
 
-	const handleCreateEvent = () => {};
-
-	const handleDayPress = (day: Day): void => {
-		if (showEventForm) {
-			setShowEventForm(false);
-		}
-		onDayPress(day);
-	};
+	const handleDayPress = useCallback(
+		(day: Day) => {
+			if (showEventForm) setShowEventForm(false);
+			onDayPress(day);
+		},
+		[showEventForm, onDayPress]
+	);
 
 	return (
-		<SafeAreaView style={{ flex: 1, width: '100%' }}>
-			<ScrollView showsVerticalScrollIndicator={false}>
-				<CalendarComponent
-					onDayPress={handleDayPress}
-					markedDates={markedDates}
+		<SafeAreaView style={homePageStyles.container}>
+			<View style={homePageStyles.contentContainer}>
+				<ButtonGroup
+					resetAllEvents={resetAllEvents}
+					createPastEvents={createPastEvents}
 				/>
-				<EventListComponent
-					selectedDateEvents={selectedDateEvents}
-					onCreateEvent={handleCreateEvent}
-					showEventForm={showEventForm}
-					setShowEventForm={setShowEventForm}
-					selectedDate={selectedDate}
-				/>
-			</ScrollView>
+				<ScrollView
+					showsVerticalScrollIndicator={false}
+					contentContainerStyle={homePageStyles.scrollContent}
+					accessibilityLabel='Calendar and events list'
+				>
+					<CalendarComponent
+						onDayPress={handleDayPress}
+						markedDates={markedDates}
+					/>
+					<EventListComponent
+						showEventForm={showEventForm}
+						setShowEventForm={setShowEventForm}
+						selectedDate={selectedDate}
+					/>
+				</ScrollView>
+			</View>
 		</SafeAreaView>
 	);
 }
